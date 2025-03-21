17482254206f1d28973fbd1f74bffc932e9bb804
K-Kazuyuki <kisidakazuyuki@gmail.com>
2025-03-22 00:04:35 +0900

    初期設定: Vite、React、TypeScript プロジェクトのセットアップ

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Footer from "./Footer";
+import Header from "./Header";
+import Body from "./Body";
+
+const App: React.FC = () => {
+  return (
+    <Box>
+      <Header />
+      <Body />
+      <Footer />
+    </Box>
+  );
+};
+
+export default App;
---
+++ src/Body.tsx
@@ -0,0 +1,15 @@
+import { Typography, Container } from "@mui/material";
+import "swiper/swiper-bundle.css";
+import "swiper/swiper.min.css";
+
+const Body = () => {
+  return (
+    <Container>
+      <Typography variant="h5">
+        この Web サイトはテーブルトーク集団最高裁判所の Web ページです．
+      </Typography>
+    </Container>
+  );
+};
+
+export default Body;
